2c547367adc1036228c77fce3c4b65b170da3290
aadityasohani <aadityasohani@outlook.com>
2021-05-27 02:13:46 +0530

    second problem 27-05-2021

--- dailyByteProblem2isPalindrome.cpp
+++ dailyByteProblem2isPalindrome.cpp
@@ -1,3 +1,12 @@
+/*Given a string, return whether or not it forms a palindrome ignoring case and non-alphabetical characters.
+Note: a palindrome is a sequence of characters that reads the same forwards and backwards.
+
+Ex: Given the following strings...
+
+"level", return true
+"algorithm", return false
+"A man, a plan, a canal: Panama.", return true*/
+
 #include<bits/stdc++.h>
 #define ll long long
 #define M 1000000007
